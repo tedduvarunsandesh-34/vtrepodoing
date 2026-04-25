bd6890997903941284dbad6563108a123425836e
tedduvarunsandesh-34 <tedduvarun376@gmail.com>
2026-04-25 18:25:12 +0530

    Update files_utils.py

--- bot/helper/ext_utils/files_utils.py
+++ bot/helper/ext_utils/files_utils.py
@@ -96,6 +96,25 @@ FIRST_SPLIT_REGEX = (
 
 SPLIT_REGEX = r"\.r\d+$|\.7z\.\d+$|\.z\d+$|\.zip\.\d+$|\.part\d+\.rar$"
 
+async def move_and_merge(source, destination, mid):
+    if not await aiopath.exists(destination):
+        await makedirs(destination, exist_ok=True)
+    for item in await listdir(source):
+        item = item.strip()
+        src_path = f"{source}/{item}"
+        dest_path = f"{destination}/{item}"
+        if await aiopath.isdir(src_path):
+            if await aiopath.exists(dest_path):
+                await move_and_merge(src_path, dest_path, mid)
+            else:
+                await move(src_path, dest_path)
+        else:
+            if item.endswith((".aria2", ".!qB")):
+                continue
+            if await aiopath.exists(dest_path):
+                dest_path = f"{destination}/{mid}-{item}"
+            await move(src_path, dest_path)
+            
 
 def is_first_archive_split(file):
     return bool(re_search(FIRST_SPLIT_REGEX, file.lower(), I))
