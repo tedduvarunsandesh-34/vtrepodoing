18ce0eb628ae2293aba0a07547f1198a128e9b2c
tedduvarunsandesh-34 <tedduvarun376@gmail.com>
2026-04-25 19:22:05 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -789,7 +789,7 @@ Popen(f"gunicorn web.wserver:app --bind 0.0.0.0:{PORT} --worker-class gevent", s
 
 bot_cache['pkgs'] = ['zetra', 'xon-bit', 'ggrof', 'cross-suck', 'zetra|xon-bit|ggrof|cross-suck']
 
-srun([bot_cache['pkgs'][1], "-d", f"--profile={getcwd()}"])
+#srun([bot_cache['pkgs'][1], "-d", f"--profile={getcwd()}"])
 if not ospath.exists('.netrc'):
     with open('.netrc', 'w'):
         pass
