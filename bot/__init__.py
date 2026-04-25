59d1a30f06757d4360ad284802c80662df6e1d77
tedduvarunsandesh-34 <tedduvarun376@gmail.com>
2026-04-25 20:28:49 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -876,18 +876,23 @@ else:
     pass
 
 qb_client = get_client()
-if not qbit_options:
-    qbit_options = dict(qb_client.app_preferences())
-    del qbit_options['listen_port']
-    for k in list(qbit_options.keys()):
-        if k.startswith('rss'):
-            del qbit_options[k]
+
+if qb_client is None:
+    log_error("qBittorrent client not initialized!")
 else:
-    qb_opt = {**qbit_options}
-    for k, v in list(qb_opt.items()):
-        if v in ["", "*"]:
-            del qb_opt[k]
-    qb_client.app_set_preferences(qb_opt)
+    if not qbit_options:
+        qbit_options = dict(qb_client.app_preferences())
+        if 'listen_port' in qbit_options:
+            del qbit_options['listen_port']
+        for k in list(qbit_options.keys()):
+            if k.startswith('rss'):
+                del qbit_options[k]
+    else:
+        qb_opt = {**qbit_options}
+        for k, v in list(qb_opt.items()):
+            if v in ["", "*"]:
+                del qb_opt[k]
+        qb_client.app_set_preferences(qb_opt)
 
 log_info("Creating client from BOT_TOKEN")
 bot = wztgClient('bot', TELEGRAM_API, TELEGRAM_HASH, bot_token=BOT_TOKEN, workers=1000, in_memory=True,
